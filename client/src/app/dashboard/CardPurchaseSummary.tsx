7e19097cb9d80524a4e933a16d3295659e57f22d
Ricardo Araya <rica.araya.f@gmail.com>
2024-11-12 17:34:13 +0100

    initial commit

---
+++ client/src/app/dashboard/CardPurchaseSummary.tsx
@@ -0,0 +1,110 @@
+import { useGetDashboardMetricsQuery } from "@/state/api";
+import { TrendingDown, TrendingUp } from "lucide-react";
+import numeral from "numeral";
+import React from "react";
+import {
+  Area,
+  AreaChart,
+  CartesianGrid,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+const CardPurchaseSummary = () => {
+  const { data, isLoading } = useGetDashboardMetricsQuery();
+  const purchaseData = data?.purchaseSummary || [];
+
+  const lastDataPoint = purchaseData[purchaseData.length - 1] || null;
+
+  return (
+    <div className="flex flex-col justify-between row-span-2 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1 bg-white shadow-md rounded-2xl">
+      {isLoading ? (
+        <div className="m-6">Loading...</div>
+      ) : (
+        <>
+          {/* HEADER */}
+          <div>
+            <h2 className="text-lg font-semibold mb-2 px-7 pt-5">
+              Purchase Summary
+            </h2>
+            <hr />
+          </div>
+
+          {/* BODY */}
+          <div>
+            {/* BODY HEADER */}
+            <div className="mb-4 mt-7 px-7">
+              <p className="text-xs text-gray-400">Purchased</p>
+              <div className="flex items-center">
+                <p className="text-2xl font-bold">
+                  {lastDataPoint
+                    ? numeral(lastDataPoint.totalPurchased).format("$0.00a")
+                    : "0"}
+                </p>
+                {lastDataPoint && (
+                  <p
+                    className={`text-sm ${
+                      lastDataPoint.changePercentage! >= 0
+                        ? "text-green-500"
+                        : "text-red-500"
+                    } flex ml-3`}
+                  >
+                    {lastDataPoint.changePercentage! >= 0 ? (
+                      <TrendingUp className="w-5 h-5 mr-1" />
+                    ) : (
+                      <TrendingDown className="w-5 h-5 mr-1" />
+                    )}
+                    {Math.abs(lastDataPoint.changePercentage!)}%
+                  </p>
+                )}
+              </div>
+            </div>
+            {/* CHART */}
+            Chart here
+            <ResponsiveContainer width="100%" height={200} className="p-2">
+              <AreaChart
+                width={100}
+                height={100}
+                data={purchaseData}
+                margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+              >
+                <defs>
+                  <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                    <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                    <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+                  </linearGradient>
+                  <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                    <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+                    <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+                  </linearGradient>
+                </defs>
+                <XAxis dataKey="name" />
+                <YAxis />
+                <CartesianGrid strokeDasharray="3 3" />
+                <Tooltip />
+                <Area
+                  type="monotone"
+                  dataKey="uv"
+                  stroke="#8884d8"
+                  fillOpacity={1}
+                  fill="url(#colorUv)"
+                />
+                <Area
+                  type="monotone"
+                  dataKey="pv"
+                  stroke="#82ca9d"
+                  fillOpacity={1}
+                  fill="url(#colorPv)"
+                />
+              </AreaChart>
+            </ResponsiveContainer>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default CardPurchaseSummary;
