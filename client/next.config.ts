c079b621c9005cd6e13c7befe53e58f5f20c8578
Ricardo <lepulent@users.noreply.github.com>
2024-11-13 22:00:19 +0100

    Update next.config.ts

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,6 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "s3-inventorymanagement.s3.us-east-2.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
   eslint: {
     ignoreDuringBuilds: true, // Disables ESLint errors during builds
   },
